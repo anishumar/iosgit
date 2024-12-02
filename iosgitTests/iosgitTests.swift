eb8046c3156ddc231335e13a540fe991914acecb
anishumar <anishumar786@gmail.com>
2024-12-02 12:44:38 +0530

    Initial Commit

---
+++ iosgitTests/iosgitTests.swift
@@ -0,0 +1,17 @@
+//
+//  iosgitTests.swift
+//  iosgitTests
+//
+//  Created by Anish Umar on 02/12/24.
+//
+
+import Testing
+@testable import iosgit
+
+struct iosgitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
